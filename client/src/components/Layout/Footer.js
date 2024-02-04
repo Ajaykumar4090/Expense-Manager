50c41dbbab66d23c2a589aba6b29901a69240ffd
Ajay Kumar <ajakumar4492@gmail.com>
2024-02-04 16:00:25 +0530

    First commit

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-dark text-light p-4 position-fixed w-100 bottom-0">
+      <h6 className="text-center text-white">
+        All rights reserved &copy; | Developed by Ajay Kumar
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
